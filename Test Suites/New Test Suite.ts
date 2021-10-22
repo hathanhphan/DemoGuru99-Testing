340d0829d91f220c1c7df972419d50ac971b0d9c
tringuyen <trinv60cntt@gmail.com>
2021-10-22 15:30:37 +0700

    add TC and Test Suite new

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>048bed28-4bd6-4b57-a9ec-c48ec074da12</testSuiteGuid>
+</TestSuiteEntity>
